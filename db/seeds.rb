451dc3d2c8618b7f2300dac2df3881176f0bdb1c
Katie Belanger <belangeerkat@Katies-MacBook-Air.local>
2020-03-03 15:30:45 -0700

    added artist instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
-nirvana = Artist.create(name: "Nirvana", age: 33, 12)
+maroon5 = Artist.create(name: "Maroon 5")
+lady_gaga = Artist.create(name: "Lady Gaga")
+christina_aguilera = Artist.create(name: "Christina Aguilera")
+bruno_mars = Artist.create(name: "Bruno Mars")
+pearl_jam = Artist.create(name: "Pearl Jam")
+pink_floyd = Artist.create(name: "Pink Floyd")
+nirvana = Artist.create(name: "Nirvana")
+ac_dc = Artist.create(name: "AC/DC")
+led_zeppelin = Artist.create(name: "Led Zeppelin")
+
+
