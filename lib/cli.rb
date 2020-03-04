ce2c09204dc1ddf77b8614f4dc5bc6ef29a7cc1a
alohajt <alohajiatao@gmail.com>
2020-03-04 14:59:29 -0700

    sli selection

--- lib/cli.rb
+++ lib/cli.rb
@@ -9,19 +9,53 @@ class Cli
         new_user = User.create({user_name:user})
     end
 
-    def pick_genre
-        prompt = TTY::Prompt.new
-        choice = prompt.select("Please choose a Genre:", %w(Pop Rock Dance Hip-Hop Country))
+    def prompt
+        TTY::Prompt.new
+    end
+
+    def genre_selection
+        Genre.all.map do |genre|
+            genre.genre_name
+        end
+        #Genre.all.map(&:genre_name)
+    end
+    
+
+    def pick_favorites
+        choice = prompt.select("Please choose a Genre:", genre_selection)
+        # %w(Pop Rock Dance Hip-Hop/Rap Country))
         # =>
         # Choose your destiny? (Use ↑/↓ arrow keys, press Enter to select)
         # ‣ Pop
         # Rock
         # Dance
 
-        #if choice == "Pop"
+        favorite_genre = Genre.all.find do |genre|
+                genre.genre_name == choice
+            end
+        artist_selection = favorite_genre.artists.map do |artist|
+            artist.name
+        end
 
+        # if choice == "Pop"
+        #     pop_choice = prompt.select("Please choose an artist you like in this genre:", artist_selection)
+        # elsif choice == "Rock"
+        #     rock_choice = prompt.select("Please choose an artist you like in this genre:", artist_selection)
+        # elsif choice == "Dance"
+        #     dance_choice = prompt.select("Please choose an artist you like in this genre:", artist_selection)
+        # elsif choice == "Hip-Hop/Rap"
+        #     hip_hop_rap_choice = prompt.select("Please choose an artist you like in this genre:", artist_selection)
+        # else #choice == "Country"
+        #     country_choice = prompt.select("Please choose an artist you like in this genre:", artist_selection)
+        # end
+        genre_choice = prompt.select("Please choose an artist you like in this genre:", artist_selection)
+
+        favorite_artist = Artist.all.find do |artist|
+            artist.name == genre_choice
+        end
+        song_selection = favorite_artist.songs.map do |song|
+            song.name
+        end
     end
-
-
 
 end
