8253a82bb173d1c8ed39d70853964d6ffb49fd4f
alohajt <alohajiatao@gmail.com>
2020-03-06 10:43:50 -0700

    changes2

--- lib/cli.rb
+++ lib/cli.rb
@@ -61,13 +61,7 @@ class Cli
 
     def view_favorite_genres  #view all favorite genres from that user
         #we want User_instantce.genre
-        # Favorite.all.select do |favorite|
-        #     favorite.genre == @genre_choice_result
-        # end
-
-        @new_user.genres
-        #binding.pry
-        user_artist_selection
+        @new_user.genres.map(&:genre_name) 
     end
 
     # def user_artist_selection(genre_choice)
@@ -77,7 +71,7 @@ class Cli
         @user_genre_selection_result = prompt.select("Please choose a Genre:", genre_names)
     end
 
-    def user_artist_selection
+    def user_artist_selections 
         @user_artist_selection_result = prompt.select("Please add this genre to your favorites or choose an artist you like in this genre:", genre_artists(@user_genre_selection_result)+add_favorite_option+my_favorites+main_menu_option)
         user_artist_and_song_selections
     end
@@ -90,18 +84,21 @@ class Cli
             puts "Added to Favorites!"
             add_favorite(genre_selection(@user_genre_selection_result))
             #user_artist_selection = @user_genre_selection_result
-            user_artist_selection
+            user_artist_selections 
+
         elsif user_artist_selection == main_menu_option.join
             user_genre_selection
-            user_artist_selection
+            user_artist_selections
         elsif user_artist_selection == my_favorites.join
-            view_favorite_genres
-            user_artist_selection
+            #binding.pry
+
+            puts view_favorite_genres
+            user_artist_selections
         else
             user_song_selection = prompt.select("Here are #{user_artist_selection}'s Top Two Songs!", artist_songs(@user_artist_selection_result)+main_menu_option)
             if user_song_selection == main_menu_option.join
                 user_genre_selection
-                user_artist_selection
+                user_artist_selections 
             end
         end
     end
--- runner.rb
+++ runner.rb
@@ -5,4 +5,4 @@ my_app = Cli.new
 
 my_app.welcome
 my_app.user_genre_selection
-my_app.user_artist_selection
+my_app.user_artist_selections
