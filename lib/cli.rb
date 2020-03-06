395f054a9f96603f4e407a987dd64ebaa6bc2c16
alohajt <alohajiatao@gmail.com>
2020-03-05 18:50:22 -0700

    fixes

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,6 +4,7 @@ class Cli
     attr_reader :new_user
 
     def welcome
+        #welcome_pic = system("imgcat lib/StockSnap_QVGSXCGO6J.jpg")
         puts "Welcome to Discover Artist Music!"
         puts "Create a user name"
 
@@ -35,8 +36,12 @@ class Cli
         ["Return to Main Menu"]
     end
 
+    def my_favorites
+        ["View my favorites"]
+    end
+
     def add_favorite(genre)
-        Favorite.create(user: new_user, genre: genre)
+        Favorite.create(user: new_user, genre: genre) 
     end
     
     def genre_artists(choice)
@@ -54,21 +59,45 @@ class Cli
         end
     end
 
-    def pick_favorites
-        choice = prompt.select("Please choose a Genre:", genre_names)
-        artist_choice = prompt.select("Please add this genre to your favorites or choose an artist you like in this genre:", genre_artists(choice)+favorite_option+main_menu_option)
+    # def view_favorite_genres
+    #     Favorite.user
+    
+    # end
+
+    # def artist_choice_prompt(genre_choice)
+    #     prompt.select("Please add this genre to your favorites or choose an artist you like in this genre:", genre_artists(genre_choice)+ favorite_option + main_menu_option)
+    # end
+    def genre_choice
+        @genre_choice_result = prompt.select("Please choose a Genre:", genre_names)
+    end
+
+    def artist_choice_prompt
+        @artist_choice_result = prompt.select("Please add this genre to your favorites or choose an artist you like in this genre:", genre_artists(@genre_choice_result)+favorite_option+main_menu_option)
+        artist_choice_happy
+    end
+
+    def artist_choice_happy
+        #genre_choice = prompt.select("Please choose a Genre:", genre_names)
+        artist_choice =  @artist_choice_result
         
         if artist_choice == favorite_option.join
             puts "Added to Favorites!"
-            add_favorite(genre_selection(choice))
-            artist_choice
+            add_favorite(genre_selection(@genre_choice_result))
+            #artist_choice = @genre_choice_result
+            artist_choice_prompt
         elsif artist_choice == main_menu_option.join
-            pick_favorites
+            genre_choice
+            artist_choice_prompt
+            
         else
-            song_choice = prompt.select("Here are #{artist_choice}'s Top Two Songs!", artist_songs(artist_choice)+main_menu_option)
+            song_choice = prompt.select("Here are #{artist_choice}'s Top Two Songs!", artist_songs(@artist_choice_result)+main_menu_option)
             if song_choice == main_menu_option.join
-                pick_favorites
+                genre_choice
+                artist_choice_prompt
+                
             end
         end
     end
+
+
 end
